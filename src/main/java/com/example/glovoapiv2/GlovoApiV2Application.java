8fa7079ccef9e98352e67c6714886fdfa5f5f163
rudendan <ruden.dan@gmail.com>
2024-01-04 17:36:55 +0200

    Initial commit

---
+++ src/main/java/com/example/glovoapiv2/GlovoApiV2Application.java
@@ -0,0 +1,13 @@
+package com.example.glovoapiv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoApiV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlovoApiV2Application.class, args);
+    }
+
+}
