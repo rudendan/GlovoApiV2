a3343994ad68f7321a716d262b3b1f05cd120077
rudendan <ruden.dan@gmail.com>
2024-01-06 22:54:26 +0200

    06.01.2024: productData.sql was added. It`s a "dummy"-data`s for product table.

---
+++ src/main/resources/productData.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, cost) VALUES
+                                      ('Product A', 20.0),
+                                      ('Product B', 15.5),
+                                      ('Product C', 30.0),
+                                      ('Product D', 10.0),
+                                      ('Product E', 25.0);
